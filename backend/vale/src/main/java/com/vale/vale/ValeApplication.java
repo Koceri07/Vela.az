ab01730963bbf13468072e47a8e896c03b8b3457
kocerimustafayev <kocerimustafayev07@gmail.com>
2026-02-23 01:30:44 +0400

    firs commit

---
+++ backend/vale/src/main/java/com/vale/vale/ValeApplication.java
@@ -0,0 +1,13 @@
+package com.vale.vale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValeApplication.class, args);
+	}
+
+}
