16272008dc6c482f74759cfd3efea33a26aecd58
Ali <aliiqafar@gmail.com>
2026-02-24 20:41:45 +0400

    initial commit: Vela proyektinin təməli

---
+++ src/main/java/com/example/velabackend/VelaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.velabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VelaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/velabackend/VelaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.velabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
