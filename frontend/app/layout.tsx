5eceb700c70ab93b29c178e903a5d92b47b86886
musazadeaysun <musazadeaysun4@gmail.com>
2026-03-16 00:38:38 +0400

    commit

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -10,13 +10,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="az">
-      <body className="flex flex-col min-h-screen" 
-          cz-shortcut-listen="true">
-        <Header />
-        <main className="flex-1">
-          {children}
-        </main>
-        <Footer />
+      <body className="flex flex-col min-h-screen" cz-shortcut-listen="true">
+        <CartProvider>
+          <Header />
+          <main className="flex-1">
+            {children}
+          </main>
+          <Footer />
+        </CartProvider>
       </body>
     </html>
   );
